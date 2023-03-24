a1339afe5e8ae4e29e1132f97b5c761840370508
zdn-Nhat-Linh <linh.nguyen@zodinet.tech>
2023-03-24 16:25:19 +0700

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head />
-			<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 			<body>
 				<Main />
 				<NextScript />
