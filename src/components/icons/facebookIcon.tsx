457e1bb7ab0f528472addab2c97119ed7780ca54
zdn-Nhat-Linh <linh.nguyen@zodinet.tech>
2023-04-04 12:41:05 +0700

    fix: 0404 v1.2.2 dmmn

--- src/components/icons/facebookIcon.tsx
+++ src/components/icons/facebookIcon.tsx
@@ -1,17 +1,17 @@
 import { SVGProps } from "react";
 
 const FacebookIcon = (props: SVGProps<SVGSVGElement>) => (
-	<svg width={33} height={32} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-		<g clipPath="url(#a)">
-			<rect x={0.5} width={32} height={32} rx={16} fill="#7A838C" />
+	<svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g clip-path="url(#clip0_50_375)">
+			<rect x="0.5" width="32" height="32" rx="16" fill="#7A838C" />
 			<path
-				d="M18.672 32V20.094h3.34l.442-4.169h-3.782l.006-2.086c0-1.088.103-1.67 1.665-1.67h2.087V8h-3.34c-4.01 0-5.423 2.022-5.423 5.423v2.502h-2.5v4.17h2.5V32H18.672Z"
-				fill="#fff"
+				d="M18.6723 32V20.0936H22.0112L22.4538 15.9251H18.6723L18.6779 13.8387C18.6779 12.7514 18.7812 12.1691 20.3428 12.1691H22.4304V8H19.0907C15.0791 8 13.6673 10.022 13.6673 13.423V15.9255H11.1666V20.0943H13.6673V32C15.1666 32 15.4687 32 16.5 32C17.1661 32 17.8333 32 18.6723 32Z"
+				fill="white"
 			/>
 		</g>
 		<defs>
-			<clipPath id="a">
-				<rect x={0.5} width={32} height={32} rx={16} fill="#fff" />
+			<clipPath id="clip0_50_375">
+				<rect x="0.5" width="32" height="32" rx="16" fill="white" />
 			</clipPath>
 		</defs>
 	</svg>
